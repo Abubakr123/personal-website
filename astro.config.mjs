83eedc3994b028f3aa7539d260d8e998e60e921b
Abubakr123 <your.email@example.com>
2025-07-20 03:57:07 +0200

    Fixed GitHub Pages deployment issues

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,11 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  site: 'https://www.abubakryagob.com',
+  site: 'https://abubakryagob.github.io',
   base: '/',
   build: {
     outDir: 'docs',
+    assets: 'astro'
   },
   integrations: [tailwind()],
 });
